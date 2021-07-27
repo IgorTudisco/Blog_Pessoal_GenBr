6e2d18e7812ca77d0e17aec4f312726674d7ba3e
Igor Tudisco <igortudisco@hotmail.com>
2021-07-27 10:25:47 -0300

    Add router

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+// comocando as rotas dentro do array, funciona como um json.
+// passa o path e o componente.
+// o Path vazio vai ser o meu redirecionamento. (Quando não tiver barra, vai ser essa rota).
+// por seguranção passamos o full para ele trazer toda a nossa rota.
+
+const routes: Routes = [
+
+  {
+    path:'',
+    redirectTo: 'entrar',
+    pathMatch: 'full'
+  },
+  {
+    path:'entrar',
+    component: EntrarComponent
+  },
+  {
+    path: 'cadastrar',
+    component: CadastrarComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
