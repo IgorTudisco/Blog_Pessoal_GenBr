d5eaa6bb5680e2d916504e1c338426dde532000c
Igor Tudisco <igortudisco@hotmail.com>
2021-07-29 12:34:23 -0300

    Criando o service e os end points de cadastrar e logar

---
+++ frontEnd/src/app/service/auth.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserLogin } from '../model/UserLogin';
+import { Observable } from 'rxjs'
+import { Usuario } from '../model/Usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+  // Fazendo a injeção de dependencia do HttpClientModule direto no meu construtor
+  // O mesmo é passado como parâmetro.
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  // Métodos principais de autenticação
+
+  // Passando os meus parâmetros para o meu entrar.
+
+  // Temos que indicar qual o tipo de dado que o Angular deve "ficar de olho"
+  // e assim garantir que o dado passado será do tipo UserLogin.
+
+  entrar(userLogin: UserLogin): Observable<UserLogin>{
+
+    // Passando a minha rota do back end para fazer o login do usuário.
+
+    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar',userLogin)
+
+  }
+
+  cadastrar(usuario: Usuario): Observable<Usuario>{
+
+    return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar',usuario)
+
+  }
+
+}
