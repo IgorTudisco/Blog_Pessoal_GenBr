94152cafc7b5c184f9a0eec2ea86b88c58c7e7ab
Igor Tudisco <igortudisco@hotmail.com>
2021-08-30 11:57:06 -0300

    Criando o CRUD de postagem

--- frontEnd/src/app/inicio/inicio.component.ts
+++ frontEnd/src/app/inicio/inicio.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Theme } from '../model/Theme';
+import { Usuario } from '../model/Usuario';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,23 +14,100 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  listaPostagem: Postagem[]
+
+  listaTheme: Theme[]
+  idTheme: number
+  theme: Theme = new Theme()
+
+  user: Usuario = new Usuario()
+
+  idUser = environment.id
+
   constructor(
-    
+
     // Injetando a minha dependencia de Router.
 
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService
 
   ) { }
 
   ngOnInit() {
 
-    if(environment.token == ''){
+    if (environment.token == '') {
 
-     // alert('Sua seção expirou, faça o login novamente.')
+      // alert('Sua seção expirou, faça o login novamente.')
 
       this.router.navigate(['/entrar'])
 
     }
+
+    this.getAllTema()
+
+  }
+
+  getAllTema() {
+
+    this.temaService.getAllTema().subscribe((resp: Theme[]) => {
+
+      this.listaTheme = resp
+
+    })
+
+  }
+
+  findByIdTheme() {
+
+    this.temaService.getById(this.idTheme).subscribe((resp: Theme) => {
+
+      this.theme = resp
+
+    })
+
+  }
+
+  findAllPostagem() {
+
+    this.postagemService.getAllPostagem().subscribe((resp: Postagem[]) => {
+
+      this.listaPostagem = resp
+
+    })
+
+  }
+
+  publicar() {
+
+    // Preenchendo os campos que não são passados pelo usuário
+
+    this.theme.id = this.idTheme
+
+    // Após passar o id do tema, colocamos ele dentro de postagem.
+
+    this.postagem.theme = this.theme
+
+    // Passando o Id do usuário para dentro do obj usuário.
+
+    this.user.id = this.idUser
+
+    // Passando o usuário para dentro de postagem.
+
+    this.postagem.usuarioPostagem = this.user
+
+    // Agora passando a postagem para o meu back.
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+
+      this.postagem = resp
+
+      alert('Postagem realizada com sucesso!')
+
+      this.postagem = new Postagem()
+
+    })
 
   }
 
---
+++ frontEnd/src/app/service/postagem.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagemService {
+
+  constructor(
+
+    private http: HttpClient
+
+  ) { }
+
+  // Colocando o Authorization e o token do usuário no header. Assim consigo fazer minha validação.
+
+  token = {
+
+    headers: new HttpHeaders().set('Authorization', environment.token)
+
+  }
+
+  getAllPostagem(): Observable<Postagem[]> {
+
+    return this.http.get<Postagem[]>('localhost:8080/postagens', this.token)
+
+  }
+
+  postPostagem(postagem: Postagem): Observable<Postagem> {
+
+    return this.http.post<Postagem>('localhost:8080/postagens', postagem, this.token)
+
+  }
+
+}
